971ecab7710c86227781d29486516331c47f073a
Thomas Butyn <thomas@ThomasLaptop.home>
2025-08-01 12:21:25 -0400

    adding bugs with rigidbodies

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Mathematics;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public GameObject body;
+    public GameObject hand;
+    public GameObject foot;
+    public GameObject occupiedAudience;
+    public GameObject startButton;
+    public GameObject curtain;
+    private float InputForce = 100.0f;
+    
+    List<GameObject> bugs = new List<GameObject>();
+    List<Rigidbody> rigidBodies = new List<Rigidbody>();
+
+    List<GameObject> bodys = new List<GameObject>();
+    List<GameObject> leftHands = new List<GameObject>();
+    List<GameObject> rightHands = new List<GameObject>();
+    List<GameObject> leftFoots = new List<GameObject>();
+    List<GameObject> rightFoots = new List<GameObject>();
+    List<GameObject> audience = new List<GameObject>();
+    List<GameObject> curtains = new List<GameObject>();
+
+    void Start()
+    {
+        for (int bugIndex = 0; bugIndex < 100; bugIndex++)
+        {
+            int row = bugIndex / 10;
+            int column = bugIndex % 10; 
+
+            float xPosition = -12.0f + 6.0f * column / 9.0f;
+            float yPosition = 1.0f;
+            float zPosition = 3.0f + 6.0f * row / 9.0f;
+            
+
+            bugs.Add(new GameObject("bug" + bugIndex.ToString()));
+            bugs[bugIndex].transform.position = new Vector3(xPosition, yPosition, zPosition);
+            SphereCollider sphereCollider = bugs[bugIndex].AddComponent<SphereCollider>();
+            sphereCollider.radius = 0.025f;
+
+            rigidBodies.Add(bugs[bugIndex].AddComponent<Rigidbody>());
+            rigidBodies[bugIndex].useGravity = true;
+            rigidBodies[bugIndex].mass = 1f;
+            rigidBodies[bugIndex].isKinematic = false;
+
+            bodys.Add(Instantiate(body));
+            leftHands.Add(Instantiate(hand));
+            rightHands.Add(Instantiate(hand));
+            leftFoots.Add(Instantiate(foot));
+            rightFoots.Add(Instantiate(foot));
+        }
+
+        // backrow
+        for (int audienceIndex = 0; audienceIndex < 18; audienceIndex++)
+        {
+            float xPosition = -1.2f + audienceIndex * 0.14f;
+            audience.Add(Instantiate(occupiedAudience));
+            audience[audienceIndex].transform.parent = mainCamera.transform;
+            audience[audienceIndex].transform.localPosition = new Vector3(xPosition, -0.5f, 0.5f);
+            audience[audienceIndex].transform.localRotation = Quaternion.identity;
+            audience[audienceIndex].transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        // middlerow
+        for (int audienceIndex = 18; audienceIndex < 18 + 20; audienceIndex++)
+        {
+            float xPosition = -1.35f + (audienceIndex - 18) * 0.14f;
+            audience.Add(Instantiate(occupiedAudience));
+            audience[audienceIndex].transform.parent = mainCamera.transform;
+            audience[audienceIndex].transform.localPosition = new Vector3(xPosition, -0.54f, 0.55f);
+            audience[audienceIndex].transform.localRotation = Quaternion.identity;
+            audience[audienceIndex].transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        // frontrow
+        for (int audienceIndex = 18 + 20; audienceIndex < 18 + 20 + 22; audienceIndex++)
+        {
+            float xPosition = -1.5f + (audienceIndex - 18 - 20) * 0.14f;
+            audience.Add(Instantiate(occupiedAudience));
+            audience[audienceIndex].transform.parent = mainCamera.transform;
+            audience[audienceIndex].transform.localPosition = new Vector3(xPosition, -0.58f, 0.6f);
+            audience[audienceIndex].transform.localRotation = Quaternion.identity;
+            audience[audienceIndex].transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+
+        for (int curtainIndex = 0; curtainIndex < 2; curtainIndex++)
+        {
+            curtains.Add(Instantiate(curtain));
+            curtains[curtainIndex].transform.parent = mainCamera.transform;
+            curtains[curtainIndex].transform.localRotation = Quaternion.identity;
+        }
+        curtains[0].transform.localPosition = new Vector3(-3.0f, -2.2f, 2.2f);
+        curtains[1].transform.localPosition = new Vector3(3.0f, -2.2f, 2.2f);
+
+        Button button = startButton.GetComponent<Button>();
+        button.onClick.AddListener(() => StartCoroutine(OpenCurtains()));
+    }
+
+    //Debug.Log(currentTime.ToString("n2"));
+    IEnumerator OpenCurtains()
+    {
+        float currentTime = 0.0f;
+        float endTime = 1.0f; 
+        while(currentTime < endTime)
+        {
+            currentTime = Mathf.Min(currentTime + Time.deltaTime, endTime);
+            curtains[0].transform.localPosition = new Vector3(-3.0f - 4.0f * currentTime, -2.2f, 2.2f);
+            curtains[1].transform.localPosition = new Vector3(3.0f + 4.0f * currentTime, -2.2f, 2.2f);
+            startButton.transform.localScale = new Vector3(1.0f - currentTime, 1.0f - currentTime, 1.0f - currentTime);
+            foreach(GameObject curtain in curtains)
+            {
+                curtain.transform.localScale = new Vector3(endTime - currentTime, 1.0f, 1.0f);
+            }
+            yield return null; // resume execution on the next frame
+        }
+    }
+
+    void Update()
+    {
+        for(int bugIndex = 0; bugIndex < 100; bugIndex++)
+        {
+            bodys[bugIndex].transform.position = bugs[bugIndex].transform.position + new Vector3(0.0f, 0.8f, 0.0f);
+            leftFoots[bugIndex].transform.position = bugs[bugIndex].transform.position + new Vector3(0.0f, 0.0f, 0.1f);
+            rightFoots[bugIndex].transform.position = bugs[bugIndex].transform.position + new Vector3(0.0f, 0.0f, -0.1f);
+            leftHands[bugIndex].transform.position = bugs[bugIndex].transform.position + new Vector3(0.0f, 0.8f, 0.1f);
+            rightHands[bugIndex].transform.position = bugs[bugIndex].transform.position + new Vector3(0.0f, 0.8f, -0.1f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            foreach (Rigidbody rb in rigidBodies)
+            {
+                rb.AddForce(new Vector3(InputForce, 0.0f, 0.0f), ForceMode.Force);
+            }
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bugs : MonoBehaviour
+{
+    float3 LeftHandReset = float3(0.1f, 1.0f, 0.0f);
+    float3 RightHadReset = float3(-0.1f, 1.0f, 0.0f);
+    float3 LeftFootReset = float3(0.1f, 0.0f, 0.0f);
+    float3 RightFootReset = float3(-0.1f, 0.0f, 0.0f);
+    float3 BodyReset = float3(0.0f, 1.0f, 0.0f);
+
+    List<bool> Exists[100];    
+    List<float3> LeftHandWorldPosition[100];
+    List<float3> RightHandWorldPosition[100];
+    List<float3> LeftFootWorldPosition[100];
+    List<float3> RightFootWorldPosition[100];
+    List<float3> BodyWorldPosition[100];
+    List<float3> TargetVelocity[100];
+    void Start()
+    {
+        for (int BugIndex = 0; BugIndex < 100; BugIndex++)
+        {
+            int Row = BugIndex / 10;
+            int Column = BugIndex % 10;
+            Exists[BugIndex] = true;
+            LeftHandWorldPosition[BugIndex] = float3(0.0f, 0.0f, 0.0f);
+            RightHandWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            LeftFootWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            RightFootWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            BodyWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            TargetVelocity[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+        }
+        BodyWorldPosition = float3(0.0, 1.0, 0.0);
+        LeftHandWorldPosition = float3()
+    }
+    void Update()
+    {
+    }
+}
+
+public class RootScript : MonoBehaviour
+{
+    string SceneToLoad;
+    public double Money = 100.0;
+    public Bugs;
+
+    const string StartScreen = "Assets/Scenes/StartScreen.unity";
+    const string Circus = "Assets/Scenes/MakeMoney.unity";
+    const string SpendMoney = "Assets/Scenes/SpendMoney.unity";
+
+    void Start()
+    {
+        SceneToLoad = StartScreen;
+        SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Additive);
+    }
+
+    void Update()
+    {
+    }
+
+    public void LoadMakeMoney()
+    {
+        SceneManager.UnloadSceneAsync(SceneToLoad);
+        SceneToLoad = MakeMoney;
+        SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Additive);
+    }
+    
+    public void LoadSpendMoney()
+    {
+        SceneManager.UnloadSceneAsync(SceneToLoad);
+        SceneToLoad = SpendMoney;
+        SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Additive);
+    }
+}
+
+public class Bugs : MonoBehaviour
+{
+    float3 LeftHandReset = float3(0.1f, 1.0f, 0.0f);
+    float3 RightHadReset = float3(-0.1f, 1.0f, 0.0f);
+    float3 LeftFootReset = float3(0.1f, 0.0f, 0.0f);
+    float3 RightFootReset = float3(-0.1f, 0.0f, 0.0f);
+    float3 BodyReset = float3(0.0f, 1.0f, 0.0f);
+
+    List<bool> Exists[100];    
+    List<float3> LeftHandWorldPosition[100];
+    List<float3> RightHandWorldPosition[100];
+    List<float3> LeftFootWorldPosition[100];
+    List<float3> RightFootWorldPosition[100];
+    List<float3> BodyWorldPosition[100];
+    List<float3> TargetVelocity[100];
+    void Start()
+    {
+        for (int BugIndex = 0; BugIndex < 100; BugIndex++)
+        {
+            int Row = BugIndex / 10;
+            int Column = BugIndex % 10;
+            Exists[BugIndex] = true;
+            LeftHandWorldPosition[BugIndex] = float3(0.0f, 0.0f, 0.0f);
+            RightHandWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            LeftFootWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            RightFootWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            BodyWorldPosition[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+            TargetVelocity[BugIndex] =  float3(0.0f, 0.0f, 0.0f);
+        }
+        BodyWorldPosition = float3(0.0, 1.0, 0.0);
+        LeftHandWorldPosition = float3()
+    }
+    void Update()
+    {
+    }
+}
+
+List<Bug> ActiveBugs;
