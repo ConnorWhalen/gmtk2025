6ed4cac54cf9fa477d77e789e4d4a2c50b8d6253
ConnorWhalen <caw742@mun.ca>
2025-08-03 10:38:57 -0230

    fix button interaction

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -370,10 +370,10 @@ public class Main : MonoBehaviour
         // Debug.Log("CAMERA RIGHT: " + bugs[0].GetComponent<SlopeDetector>().cameraRight);
 
         // Demo curtain close
-        if (Input.GetKey(KeyCode.P))
-        {
-            StartCoroutine(CloseCurtains());
-        }
+        // if (Input.GetKey(KeyCode.P))
+        // {
+        //     StartCoroutine(CloseCurtains());
+        // }
 
         bool transitionPlayingNow = GameObject.Find("SongTransition").GetComponent<AudioSource>().isPlaying;
         if (transitionPlaying && !transitionPlayingNow)
